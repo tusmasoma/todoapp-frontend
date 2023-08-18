e32816f70beed6c22b7e849f1e328b36a2ffdd4c
somak <s.k18@icloud.com>
2023-08-18 16:21:14 +0900

    step-b

--- todoapp/src/App.tsx
+++ todoapp/src/App.tsx
@@ -2,6 +2,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import { TodoListPage } from './pages/TodoListPage';
+import { getTask } from './apis/getTask';
 
 export const App: React.FC = () => {
 
