edfbfa25934b2ae8777829934702941349f079bd
somak <s.k18@icloud.com>
2023-08-19 20:26:15 +0900

    data変数をTodoList変数に変更

--- todoapp/src/pages/TodoListPage.tsx
+++ todoapp/src/pages/TodoListPage.tsx
@@ -6,25 +6,25 @@ import { TodoItem } from "../components/molecules/TodoItem/TodoItem"
 import { getTask } from '../apis/getTask';
 
 export const TodoListPage:React.FC = () => {
-    const [data, setData] = useState<Task[] | null>(null);
+    const [TodoList, setTodoList] = useState<Task[] | null>(null);
+
+    const fetchData = async () => {
+        try {
+            const responseData:Task[] = await getTask();
+            setTodoList(responseData);
+        } catch (error:any) {
+            console.error(error);
+        }
+    };
 
     useEffect(() => {
-        // dataの取得は最初の読み込み時のみ
-        const fetchData = async () => {
-            try {
-                const responseData:Task[] = await getTask();
-                setData(responseData);
-            } catch (error:any) {
-                console.error(error);
-            }
-        };
-
+        //dataの取得は最初のページ読み込みの時のみ
         fetchData();
     }, []);
 
     return (
         <TodoListContainer maxWidth="sm">
-            {data ? data.map( item => (
+            {TodoList ? TodoList.map( item => (
             <TodoItem key={item.id} {...item} />
             )):
             (<p>loading</p>
