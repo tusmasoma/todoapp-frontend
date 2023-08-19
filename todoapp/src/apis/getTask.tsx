b60f925b8518b3c494f1a7e0ae1b07b66dd2391f
somak <s.k18@icloud.com>
2023-08-19 16:40:34 +0900

    step-aの修正点の反映

---
+++ todoapp/src/apis/getTask.tsx
@@ -0,0 +1,12 @@
+
+import axios from 'axios';
+import { Task } from '../types/task';
+
+export const getTask = async () => {
+    try {
+        const response = await axios.get<Task[]>('http://127.0.0.1:8000/task/?format=json');
+        return response.data;
+    } catch (error: any) {
+        throw new Error(error);
+    }
+};
--- todoapp/src/pages/TodoListPage.tsx
+++ todoapp/src/pages/TodoListPage.tsx
@@ -1,31 +1,34 @@
 import React from 'react';
+import { useState, useEffect } from "react"
 import type { Task } from '../types/task';
 import { TodoListContainer } from "../components/atoms/Container/Container.styles"
 import { TodoItem } from "../components/molecules/TodoItem/TodoItem"
-
+import { getTask } from '../apis/getTask';
 
 export const TodoListPage:React.FC = () => {
-    const data: Task[] = [
-        {
-            "id": "fd57699c-641e-499a-bd25-346634d6a6ce",
-            "title": "backend開発",
-            "description": "来週の月曜までに基本的なToDoアプリケーションの実装を終わらせる",
-            "created_at": "2023-08-09T09:13:59.777401Z",
-            "formatted_created_at": "2023-08-09"
-        },
-        {
-            "id": "f771a715-285b-4bcd-b97c-f38713a5ee62",
-            "title": "reactの勉強",
-            "description": "金曜までに、reactの本§６まで読んで、休日に実装する",
-            "created_at": "2023-08-09T09:14:23.060394Z",
-            "formatted_created_at": "2023-08-09"
-        }
-    ]
+    const [data, setData] = useState<Task[] | null>(null);
+
+    useEffect(() => {
+        // dataの取得は最初の読み込み時のみ
+        const fetchData = async () => {
+            try {
+                const responseData:Task[] = await getTask();
+                setData(responseData);
+            } catch (error:any) {
+                console.error(error);
+            }
+        };
+
+        fetchData();
+    }, []);
+
     return (
         <TodoListContainer maxWidth="sm">
-            {data.map( item => (
-            <TodoItem key={item.id} {...item}/>
-            ))}
+            {data ? data.map( item => (
+            <TodoItem key={item.id} {...item} />
+            )):
+            (<p>loading</p>
+            )}
         </TodoListContainer>    
     )
 }
