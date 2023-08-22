153f39a09bbd4a79f907d00708df9b20e82c99b2
somak <s.k18@icloud.com>
2023-08-22 17:39:07 +0900

    getTaskをgetTasksに変更、TodoListのstateをnull許容しないように空の配列で初期化

--- todoapp/src/pages/TodoListPage.tsx
+++ todoapp/src/pages/TodoListPage.tsx
@@ -3,14 +3,14 @@ import { useState, useEffect } from "react"
 import type { Task } from '../types/task';
 import { TodoListContainer } from "../components/atoms/Container/Container.styles"
 import { TodoItem } from "../components/molecules/TodoItem/TodoItem"
-import { getTask } from '../apis/getTask';
+import { getTasks } from '../apis/getTasks';
 
 export const TodoListPage:React.FC = () => {
-    const [TodoList, setTodoList] = useState<Task[] | null>(null);
+    const [TodoList, setTodoList] = useState<Task[]>([]);
 
     const fetchData = async () => {
         try {
-            const responseData:Task[] = await getTask();
+            const responseData:Task[] = await getTasks();
             setTodoList(responseData);
         } catch (error:any) {
             console.error(error);
