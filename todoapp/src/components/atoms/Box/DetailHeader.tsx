a1ed91e9beb523494658ee900689958fdaa7d638
somak <s.k18@icloud.com>
2023-08-25 19:26:48 +0900

    Stackのdirectionをrowに変更

--- todoapp/src/components/atoms/Box/DetailHeader.tsx
+++ todoapp/src/components/atoms/Box/DetailHeader.tsx
@@ -3,5 +3,6 @@ import { Stack } from "@mui/material";
 import { styled } from '@mui/system';
 
 export const DetailHeader = styled(Stack)({
+    flexDirection: "row",
     justifyContent: 'space-between'
 })
