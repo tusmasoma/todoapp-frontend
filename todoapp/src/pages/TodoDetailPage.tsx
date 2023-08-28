5923a5d113fa18f98e1a4203ae6cc055f70d1128
somak <s.k18@icloud.com>
2023-08-28 15:06:54 +0900

    Add UpdateTaskDialog and integrate it with TodoDetailPage

--- todoapp/src/pages/TodoDetailPage.tsx
+++ todoapp/src/pages/TodoDetailPage.tsx
@@ -10,6 +10,7 @@ import { TodoDetailTitle } from "../components/atoms/Typography/TodoDetailTitle"
 import { CreatedAt } from "../components/atoms/Typography/CreatedAt";
 import { TodoDetailContent } from "../components/atoms/Typography/TodoDetailContent";
 import { EditButton } from "../components/atoms/Button/EditButton";
+import { UpdateTaskDialog } from "../components/organisms/UpdateTaskDialog";
 
 type Params = {
     id: string;
@@ -20,6 +21,7 @@ export const TodoDetailPage: React.FC = () => {
     const { id } = useParams<Params>();
 
     const [TodoData, setTodoData] = useState<Task | null>(null);
+    const [updateTrigger, setUpdateTrigger] = useState<boolean>(false);
 
     const fetchData = async () => {
         try {
@@ -37,23 +39,34 @@ export const TodoDetailPage: React.FC = () => {
     useEffect(() => {
         //dataの取得は最初のページ読み込みの時のみ
         fetchData();
-    }, []);
+    }, [updateTrigger]);
 
+    const [UpdateModalOpen,setUpdateModalOpen] = useState<boolean>(false);
+
+    const openModal = () => {
+        setUpdateModalOpen(true);
+    }
+    const closeModal = () => {
+        setUpdateModalOpen(false);
+    }
 
     return (
         <TodoDetailContainer maxWidth="sm">
-            {TodoData ?
-                <TodoDetailPaper>
-                    <DetailHeader>
-                        <TodoDetailTitle title={TodoData.title} />
-                        <EditButton />
-                    </DetailHeader>
-                    <CreatedAt createdAt={TodoData.formatted_created_at} />
-                    <TodoDetailContent content={TodoData.description} />
-                </TodoDetailPaper>
-                : (
-                    <p>loading</p>
-                )}
+            {TodoData ? 
+            <>
+            <TodoDetailPaper>
+                <DetailHeader>
+                    <TodoDetailTitle title={TodoData.title}/>
+                    <EditButton openModal={openModal}/>
+                </DetailHeader>
+                <CreatedAt createdAt={TodoData.formatted_created_at}/>
+                <TodoDetailContent content={TodoData.description}/>
+            </TodoDetailPaper>
+            <UpdateTaskDialog {...TodoData} open={UpdateModalOpen} onClose={closeModal} setUpdateTrigger={setUpdateTrigger}/>
+            </>
+          :(
+          <p>loading</p>
+          )}
         </TodoDetailContainer>
     )
 }
