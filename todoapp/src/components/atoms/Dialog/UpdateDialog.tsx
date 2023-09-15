90ff865638e1c934f22eea117c000f592713f08b
somak <s.k18@icloud.com>
2023-09-15 13:51:02 +0900

    Addressed PR feedback

--- todoapp/src/components/atoms/Dialog/UpdateDialog.tsx
+++ todoapp/src/components/atoms/Dialog/UpdateDialog.tsx
@@ -1,24 +1,60 @@
 
 import React from "react"
-import { Dialog } from "@mui/material"
+import { Dialog, DialogTitle, DialogContent, DialogActions } from "@mui/material"
+import { CancelUpdateButton } from "src/components/atoms/Button/CancelUpdateButton"
+import { ConfirmUpdateButton } from "src/components/atoms/Button/ConfirmUpdateButton"
+import { TitleEditField } from "src/components/atoms/TextField/TitleEditField"
+import { DescriptionEditField } from "src/components/atoms/TextField/DescriptionEditField"
 
-type Props = {
-    open: boolean;
+type Props {
+    isOpen: boolean;
     onClose: () => void;
-    children: React.ReactNode;
+    title: string;
+    editedTitle: string;
+    setEditedTitle: (title: string) => void;
+    editedDescription: string;
+    setEditedDescription: (description: string) => void;
+    handleUpdate: () => void;
+    ariaLabelledBy?: string;
+    ariaDescribedBy?: string;
 }
 
-export const UpdateDialog: React.FC<Props> = ({ open, onClose, children }) => {
+export const UpdateDialog: React.FC<Props> = ({
+    isOpen,
+    onClose,
+    title,
+    editedTitle,
+    setEditedTitle,
+    editedDescription,
+    setEditedDescription,
+    handleUpdate,
+    ariaLabelledBy,
+    ariaDescribedBy,
+}) => {
+    const isDisabled = !editedTitle.trim() || !editedDescription.trim();
+
     return (
         <Dialog
-            open={open}
+            open={isOpen}
             onClose={onClose}
-            aria-labelledby="alert-dialog-title"
-            aria-describedby="alert-dialog-description"
-            fullWidth={true}
+            fullWidth
             maxWidth="sm"
+            aria-labelledby={ariaLabelledBy || 'default-label-id'}
+            aria-describedby={ariaDescribedBy || 'default-description-id'}
         >
-            {children}
-        </Dialog>
-    )
-}
+            <DialogTitle sx={{ paddingLeft: '5px' }}>{title}</DialogTitle>
+            <DialogContent sx={{
+                display: 'flex',
+                flexDirection: 'column',
+                gap: '30px',
+            }}>
+                <TitleEditField value={editedTitle} onChange={setEditedTitle} />
+                <DescriptionEditField value={editedDescription} onChange={setEditedDescription} />
+            </DialogContent>
+            <DialogActions sx={{ padding: '10px 20px' }}>
+                <CancelUpdateButton onClick={onClose} />
+                <ConfirmUpdateButton onClick={handleUpdate} disabled={isDisabled} />
+            </DialogActions>
+        </Dialog >
+    );
+};
